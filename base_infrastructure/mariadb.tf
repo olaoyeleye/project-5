356ce738213c0494ac79ffb664124b0dbcab8178
kunle <olaoyeleye@yahoo.co.uk>
2023-06-30 01:41:30 +0100

    commented the availability zones as it was failing to load maria db

--- base_infrastructure/mariadb.tf
+++ base_infrastructure/mariadb.tf
@@ -31,7 +31,7 @@ resource "aws_db_instance" "mariadb" {
   vpc_security_group_ids    = [aws_security_group.sg-1.id]
   storage_type              = "gp2"
   backup_retention_period   = 30                                          # how long you’re going to keep your backups
-  availability_zone         = [aws_subnet.Private-sub-3.availability_zone] # prefered AZ
+  #availability_zone         = [aws_subnet.Private-sub-3.availability_zone] # prefered AZ
   final_snapshot_identifier = "mariadb-final-snapshot"                    # final snapshot when executing terraform destroy
   tags = {
     Name = "mariadb-instance"
